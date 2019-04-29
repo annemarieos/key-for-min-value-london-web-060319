bd8f9b609f03ceb814e4af4facf88dcecc47528a
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-29 20:55:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,20 @@
 
 def key_for_min_value(name_hash)
 
+  if name_hash.empty? 
+    nil
+  else 
+    min_key = nil
+
+    name_hash.each_key do |key|
+      if min_key.nil? || name_hash[:key] < name_hash[:min_key] 
+        min_key = key
+      end 
+    end 
+
+    min_key
+  end
+
 end
+
+
